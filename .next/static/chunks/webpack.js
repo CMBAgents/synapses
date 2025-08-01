8e6683eb2664bfe593c3eb142186d17967307e54
chadi_aat <chadiaitek2004@gmail.com>
2025-08-01 14:20:23 +0200

    Final project reorganization - Moved configuration files to deployment-config/ - Organized scripts by function - Updated all import paths - Cleaned up root directory structure - Moved styles to app/styles/ - Centralized documentation in docs/

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -190,7 +190,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("9a874cdc25986b55")
+/******/ 		__webpack_require__.h = () => ("f71fc19db5216282")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
